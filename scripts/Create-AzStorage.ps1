5a433ba0771e9b63945da8ed9158a34e525610f5
Adam Rush <adamrushuk@gmail.com>
2020-03-01 16:51:48 +0000

    Added try/catch block

--- scripts/Create-AzStorage.ps1
+++ scripts/Create-AzStorage.ps1
@@ -3,23 +3,39 @@
 # Ensure any errors fail the build
 $ErrorActionPreference = "Stop"
 
+# Set prefs
+# TODO: make dynamic depending on $env:CI_DEBUG
+$VerbosePreference = "Continue"
+
 # Resource Group
-Write-Output "`nSTARTED: Creating Resource Group..."
+Write-Verbose "`nSTARTED: Creating Resource Group..."
 az group create --location $env:LOCATION --name $env:TERRAFORM_STORAGE_RG
-Write-Output "FINISHED: Creating Resource Group."
+
+Write-Verbose "FINISHED: Creating Resource Group."
 
 # Storage Account
-Write-Output "`nSTARTED: Creating Storage Account..."
-az storage account create --name $env:TERRAFORM_STORAGE_ACCOUNT --resource-group $env:TERRAFORM_STORAGE_RG --location $env:LOCATION --sku Standard_LRS
-Write-Output "FINISHED: Creating Storage Account."
+$taskMessage = "Creating Storage Account"
+Write-Verbose "STARTED: $taskMessage..."
+try {
+    az storage account create --name $env:TERRAFORM_STORAGE_ACCOUNT --resource-group $env:TERRAFORM_STORAGE_RG --location $env:LOCATION --sku Standard_LRS
+
+    Write-Verbose "FINISHED: $taskMessage."
+}
+catch {
+    Write-Error "ERROR: $taskMessage." -ErrorAction 'Continue'
+    throw $_
+}
+
+
+
 
 # Storage Container
-Write-Output "`nSTARTED: Creating Storage Container..."
+Write-Verbose "`nSTARTED: Creating Storage Container..."
 az storage container create --name "terraform" --account-name $env:TERRAFORM_STORAGE_ACCOUNT
-Write-Output "FINISHED: Creating Storage Container."
+Write-Verbose "FINISHED: Creating Storage Container."
 
 # Get latest supported AKS version
-Write-Output "`nSTARTED: Finding latest supported AKS version..."
+Write-Verbose "`nSTARTED: Finding latest supported AKS version..."
 $latest_aks_version = $(az aks get-versions -l $env:LOCATION --query "orchestrators[-1].orchestratorVersion" -o tsv)
-Write-Output "Latest AKS Version: [$latest_aks_version]"
-Write-Output "FINISHED: Finding latest supported AKS version.`n"
+Write-Verbose "Latest AKS Version: [$latest_aks_version]"
+Write-Verbose "FINISHED: Finding latest supported AKS version.`n"
