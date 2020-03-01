d2b9fc4d9ff3e2564ce65ccd18de0976f8522b52
Adam Rush <adamrushuk@gmail.com>
2020-03-01 17:42:35 +0000

    Enabled CI_DEBUG

--- scripts/Login-Azure.ps1
+++ scripts/Login-Azure.ps1
@@ -1,19 +1,16 @@
 # Login to Azure using Azure Service Principal env vars
 
-# Ensure any errors fail the build
+# Set preferences
+$VerbosePreference = if ($env:CI_DEBUG -eq "true") { "Continue" } else { "SilentlyContinue" }
+# Ensure any PowerShell errors fail the build (try/catch wont work for non-PowerShell CLI commands)
 $ErrorActionPreference = "Stop"
 
-# DEBUG
-if ($env:CI_DEBUG -eq "true") {
-    Get-ChildItem env: | Select-Object Name, Value
-    $VerbosePreference = "Continue"
-} else {
-    $VerbosePreference = "SilentlyContinue"
-}
+# Output all env vars
+if ($env:CI_DEBUG -eq "true") { Get-ChildItem env: | Select-Object Name, Value }
 
 # Login to Az
 $message = "Logging in to Azure"
-Write-Output "STARTED: $message..."
+Write-Verbose "STARTED: $message..."
 az login --service-principal --tenant $env:ARM_TENANT_ID -u $env:ARM_CLIENT_ID -p $env:ARM_CLIENT_SECRET | Out-Null
 az account set --subscription $env:ARM_SUBSCRIPTION_ID
-Write-Output "FINISHED: $message."
+Write-Verbose "FINISHED: $message."
