cdadd3ddbe27bcc040d37e86ef933f746572a1a5
Adam Rush <adamrushuk@gmail.com>
2020-03-03 07:28:29 +0000

    Changed to use orig TF script

--- scripts/Apply-Terraform.ps1
+++ scripts/Apply-Terraform.ps1
@@ -1,14 +1,34 @@
-# Ensure any errors fail the build
+# Terraform Apply
+
+#region Vars
+# Set preferences
+$VerbosePreference = if ($env:CI_DEBUG -eq "true") { "Continue" } else { "SilentlyContinue" }
+# Ensure any PowerShell errors fail the build (try/catch wont work for non-PowerShell CLI commands)
 $ErrorActionPreference = "Stop"
+#endregion
+
+
+
+#region Prep
+# Info
+Get-Command terraform | Select-Object Name, Source
+terraform version
 
 # Change into TF folder location
 Push-Location -Path .\terraform
+#endregion
 
+
+
+#region Terraform
 # Apply terraform
 $message = "Applying Terraform configuration"
 Write-Output "STARTED: $message..."
 terraform apply -auto-approve
 Write-Output "FINISHED: $message."
+#endregion
+
+
 
 # Revert to previous folder location
 Pop-Location
