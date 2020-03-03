00c7c850d9d26f3b510dd4e8da8a4ed539542e75
Adam Rush <adamrushuk@gmail.com>
2020-03-03 07:02:06 +0000

    Removed global output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
-output "aks_config" {
-  value = azurerm_kubernetes_cluster.aks
-}
+# output "aks_config" {
+#   value = azurerm_kubernetes_cluster.aks
+# }
 
 output "aks_browse_command" {
   value = "az aks browse --resource-group ${azurerm_kubernetes_cluster.aks.resource_group_name} --name ${azurerm_kubernetes_cluster.aks.name}"
