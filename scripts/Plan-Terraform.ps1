51adbc9d0db8c3c41f6b89d771b4a1f80221966f
Adam Rush <adamrushuk@gmail.com>
2020-03-03 07:21:26 +0000

    Added comments / info cmds

--- scripts/Plan-Terraform.ps1
+++ scripts/Plan-Terraform.ps1
@@ -1,9 +1,26 @@
-# Ensure any errors fail the build
+# Terraform Plan
+
+#region Vars
+# Set preferences
+$VerbosePreference = if ($env:CI_DEBUG -eq "true") { "Continue" } else { "SilentlyContinue" }
+# Ensure any PowerShell errors fail the build (try/catch wont work for non-PowerShell CLI commands)
 $ErrorActionPreference = "Stop"
+#endregion
+
+
+
+#region Prep
+# Info
+Get-Command terraform | Select-Object Name, Source
+terraform version
 
 # Change into TF folder location
 Push-Location -Path .\terraform
+#endregion
 
+
+
+#region Terraform
 # Plan with differential output
 $message = "Planning Terraform configuration"
 Write-Output "STARTED: $message..."
@@ -12,6 +29,9 @@ Write-Output "FINISHED: $message."
 
 Write-Output "Terraform Plan - Generated on: $(Get-Date)`n" > diff.txt
 terraform show -no-color tfplan | Tee-Object -FilePath diff.txt
+#endregion
+
+
 
 # Revert to previous folder location
 Pop-Location
