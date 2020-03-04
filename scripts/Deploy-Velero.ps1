7ffe337782b60dd84678c110982ea58450ef149b
Adam Rush <adamrushuk@gmail.com>
2020-03-04 13:54:04 +0000

    Commented out helm install

--- scripts/Deploy-Velero.ps1
+++ scripts/Deploy-Velero.ps1
@@ -4,7 +4,7 @@
 $ErrorActionPreference = "Stop"
 
 #region Velero
-https://github.com/vmware-tanzu/helm-charts/blob/master/charts/velero/README.md
+# https://github.com/vmware-tanzu/helm-charts/blob/master/charts/velero/README.md
 $message = "[HELM] Installing Velero"
 Write-Output "STARTED: $message..."
 
@@ -26,22 +26,22 @@ if ($helmReleaseName -in $helmDeployedList.Releases.Name) {
     # helm upgrade [RELEASE] [CHART] [flags]
     # helm upgrade something ./path/to/my/chart -f my-values.yaml --install --atomic
     # TODO: complete all values below
-    helm install --namespace velero `
-        --set configuration.provider=azure `
-        --set-file credentials.secretContents.cloud=<FULL PATH TO FILE> `
-        --set configuration.backupStorageLocation.name=azure `
-        --set configuration.backupStorageLocation.bucket=<BUCKET NAME> `
-        --set configuration.backupStorageLocation.config.region=uksouth `
-        --set configuration.volumeSnapshotLocation.name=azure `
-        --set configuration.volumeSnapshotLocation.config.region=uksouth `
-        --set image.repository=velero/velero `
-        --set image.tag=v1.3.0 `
-        --set image.pullPolicy=IfNotPresent `
-        --set initContainers[0].name=velero-plugin-for-microsoft-azure `
-        --set initContainers[0].image=velero/velero-plugin-for-microsoft-azure:v1.0.1 `
-        --set initContainers[0].volumeMounts[0].mountPath=/target `
-        --set initContainers[0].volumeMounts[0].name=plugins `
-        vmware-tanzu/velero
+    # helm install --namespace velero `
+    #     --set configuration.provider=azure `
+    #     --set-file credentials.secretContents.cloud=<FULL PATH TO FILE> `
+    #     --set configuration.backupStorageLocation.name=azure `
+    #     --set configuration.backupStorageLocation.bucket=velero `
+    #     --set configuration.backupStorageLocation.config.region=uksouth `
+    #     --set configuration.volumeSnapshotLocation.name=azure `
+    #     --set configuration.volumeSnapshotLocation.config.region=uksouth `
+    #     --set image.repository=velero/velero `
+    #     --set image.tag=v1.3.0 `
+    #     --set image.pullPolicy=IfNotPresent `
+    #     --set initContainers[0].name=velero-plugin-for-microsoft-azure `
+    #     --set initContainers[0].image=velero/velero-plugin-for-microsoft-azure:v1.0.1 `
+    #     --set initContainers[0].volumeMounts[0].mountPath=/target `
+    #     --set initContainers[0].volumeMounts[0].name=plugins `
+    #     vmware-tanzu/velero
 
     # [OPTIONAL] args
 }
