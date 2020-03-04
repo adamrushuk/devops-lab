fe8498f8d6afa8d27c937db53ac3954c3644793f
Adam Rush <adamrushuk@gmail.com>
2020-03-04 07:12:56 +0000

    Added error handling to login script

--- scripts/Login-Azure.ps1
+++ scripts/Login-Azure.ps1
@@ -8,9 +8,40 @@ $ErrorActionPreference = "Stop"
 # Output all env vars
 if ($env:CI_DEBUG -eq "true") { Get-ChildItem env: | Select-Object Name, Value }
 
-# Login to Az
-$message = "Logging in to Azure"
-Write-Verbose "STARTED: $message..."
-az login --service-principal --tenant $env:ARM_TENANT_ID -u $env:ARM_CLIENT_ID -p $env:ARM_CLIENT_SECRET | Out-Null
-az account set --subscription $env:ARM_SUBSCRIPTION_ID
-Write-Verbose "FINISHED: $message."
+
+
+#region Login
+$taskMessage = "Logging in to Azure"
+Write-Verbose "STARTED: $taskMessage..."
+
+# Run CLI command
+$outputJson = az login --service-principal --tenant $env:ARM_TENANT_ID -u $env:ARM_CLIENT_ID -p $env:ARM_CLIENT_SECRET | ConvertFrom-Json
+
+# Error handling
+if (-not $outputJson) {
+    Write-Error "ERROR: $taskMessage." -ErrorAction 'Continue'
+    throw $_
+} else {
+    Write-Verbose "FINISHED: $taskMessage."
+}
+#endregion
+
+
+
+#region Subscription
+$taskMessage = "Selecting Subscription"
+Write-Verbose "STARTED: $taskMessage..."
+
+# Run CLI command
+$subOutputJson = az account set --subscription $env:ARM_SUBSCRIPTION_ID | ConvertFrom-Json
+
+# Error handling
+if (-not $subOutputJson) {
+    Write-Error "ERROR: $taskMessage." -ErrorAction 'Continue'
+    throw $_
+} else {
+    $subOutputJson
+    Write-Verbose "FINISHED: $taskMessage."
+}
+#endregion
+
