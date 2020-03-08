f3030fe9bdec5f47a3f46f1471c2897e14b45c6b
Adam Rush <adamrushuk@gmail.com>
2020-03-08 07:41:13 +0000

    Added context env var

--- scripts/Get-EventData.ps1
+++ scripts/Get-EventData.ps1
@@ -1,5 +1,11 @@
-# Get GitHub event context data
-$eventContextJson = '${{ toJson( github.event ) }}'
+<#
+    # Get GitHub event context data from env var
+    # assumes GitHub workflow
+    - name: Show triggered event data
+    env:
+    GITHUB_CONTEXT: ${{ toJson(github) }}
+#>
+$eventContextJson = $env:GITHUB_CONTEXT
 
 # Convert to PowerShell object
 $eventContext = $eventContextJson | ConvertFrom-Json
