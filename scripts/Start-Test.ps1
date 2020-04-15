7abc57d60e456fa6a9fac170b3358871ab717971
Adam Rush <adamrushuk@gmail.com>
2020-04-15 07:43:37 +0100

    Changed verbose output

--- scripts/Start-Test.ps1
+++ scripts/Start-Test.ps1
@@ -18,7 +18,7 @@ $taskMessage = "Installing Pester "
 Write-Verbose "STARTED: $taskMessage..."
 try {
     Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
-    Install-Module -Name "Pester" -Scope "CurrentUser" -Force -Verbose
+    Install-Module -Name "Pester" -Scope "CurrentUser" -Force -Verbose:$VerbosePreference
 
     Write-Verbose "FINISHED: $taskMessage."
 }
