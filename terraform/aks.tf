84a59a8d660483e9f54462408a7c681637fe3b5e
Adam Rush <adamrushuk@gmail.com>
2020-04-26 08:32:45 +0100

    Added indices

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -38,8 +38,8 @@ resource "azurerm_log_analytics_solution" "aks" {
   solution_name         = "ContainerInsights"
   location              = azurerm_resource_group.aks.location
   resource_group_name   = azurerm_resource_group.aks.name
-  workspace_resource_id = azurerm_log_analytics_workspace.aks.id
-  workspace_name        = azurerm_log_analytics_workspace.aks.name
+  workspace_resource_id = azurerm_log_analytics_workspace.aks[0].id
+  workspace_name        = azurerm_log_analytics_workspace.aks[0].name
 
   plan {
     publisher = "Microsoft"
@@ -97,7 +97,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
     oms_agent {
       enabled                    = var.aks_container_insights_enabled
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.aks.id
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.aks[0].id
     }
   }
 
