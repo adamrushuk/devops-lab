545593455025b95cc3e75e65c92cb20927dd7bfe
Adam Rush <adamrushuk@gmail.com>
2020-05-02 13:54:45 +0100

    Changed pypi script to use env var input

--- ansible/roles/pypi_repo/files/upload_package.sh
+++ ansible/roles/pypi_repo/files/upload_package.sh
@@ -6,9 +6,6 @@
 set -euo pipefail
 trap "echo 'error: Script failed: see failed command above'" ERR
 
-# debug
-# printenv | sort
-
 # create package
 # cd hello
 echo -e "\ncreating package..."
@@ -23,13 +20,13 @@ twine check dist/*
 
 # publish
 echo -e "\nuploading package..."
-twine upload --username "$1" --password "$2" --repository-url https://nexus.thehypepipe.co.uk/repository/pypi-repo/ dist/*
+twine upload --username "$USERNAME" --password "$PASSWORD" --repository-url "$REPO_URL/" dist/*
 
 # install from private pypi repo
 # pip install --index-url http://my.package.repo/simple/ SomePackage
 echo -e "\ninstalling package..."
-pip install --user --index-url https://nexus.thehypepipe.co.uk/repository/pypi-repo/simple adams-package
-pip list --local | grep adams-package
+pip install --user --index-url "$REPO_URL/simple" "$PACKAGE_NAME"
+pip list --local | grep "$PACKAGE_NAME"
 
 # uninstall
-pip uninstall --yes adams-package
+pip uninstall --yes "$PACKAGE_NAME"
