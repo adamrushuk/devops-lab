5aa398e958b62cfaf37bdcd2dffa611d9d997275
Adam Rush <adamrushuk@gmail.com>
2020-05-04 07:09:45 +0100

    Fixed setup

--- ansible/roles/pypi_repo/files/hello/setup.py
+++ ansible/roles/pypi_repo/files/hello/setup.py
@@ -6,14 +6,16 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='adams-package',
-      version='1.0.0',
-      description='Adams test package',
-      long_description=long_description,
-      long_description_content_type='text/markdown'
-      author='Adam Rush',
-      author_email='adam@example.com',
-      url='https://github.com/adamrushuk/aks-nexus-velero/tree/develop/ansible/roles/pypi_repo/files/hello/',
-      license='MIT',
-      packages=['helloworld'],
-      zip_safe=False)
+setup(
+    name='adams-package',
+    version='1.0.0',
+    description='Adams test package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Adam Rush',
+    author_email='adam@example.com',
+    url='https://github.com/adamrushuk/aks-nexus-velero/tree/develop/ansible/roles/pypi_repo/files/hello/',
+    license='MIT',
+    packages=['helloworld'],
+    zip_safe=False
+)
