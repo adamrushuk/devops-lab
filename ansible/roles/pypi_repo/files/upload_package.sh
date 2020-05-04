04d1d553c8495112b529d00b66cdc8c321d0a401
Adam Rush <adamrushuk@gmail.com>
2020-05-04 07:33:23 +0100

    Changed to use v3

--- ansible/roles/pypi_repo/files/upload_package.sh
+++ ansible/roles/pypi_repo/files/upload_package.sh
@@ -8,18 +8,18 @@ trap "echo 'error: Script failed: see failed command above'" ERR
 
 # install twine
 echo "installing twine..."
-pip install --user twine
+pip3 install --user twine
 
 # info
-python --version
-pip show setuptools wheel twine
+echo "show version info..."
+python3 --version
+pip3 --version
+pip3 show setuptools wheel twine
 
 # create package
 # cd hello
 echo "creating package..."
-python setup.py sdist
-
-
+python3 setup.py sdist
 
 # check
 # twine check dist/*
@@ -29,10 +29,10 @@ echo "uploading package..."
 twine upload --username "$USERNAME" --password "$PASSWORD" --repository-url "$REPO_URL/" dist/*
 
 # install from private pypi repo
-# pip install --index-url http://my.package.repo/simple/ SomePackage
+# pip3 install --index-url http://my.package.repo/simple/ SomePackage
 echo "installing package..."
-pip install --user --index-url "$REPO_URL/simple" "$PACKAGE_NAME"
-pip list --local | grep "$PACKAGE_NAME"
+pip3 install --user --index-url "$REPO_URL/simple" "$PACKAGE_NAME"
+pip3 list --local | grep "$PACKAGE_NAME"
 
 # uninstall
-# pip uninstall --yes "$PACKAGE_NAME"
+# pip3 uninstall --yes "$PACKAGE_NAME"
