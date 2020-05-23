287c2e3918b1655774b7aa6814d141d28d578737
Adam Rush <adamrushuk@gmail.com>
2020-05-23 15:09:36 +0100

    Added Managed Identity authentication example

--- dns/azure_dns.sh
+++ dns/azure_dns.sh
@@ -8,19 +8,49 @@ set -euo pipefail
 trap "echo 'error: Script failed: see failed command above'" ERR
 
 # vars
-resource_group="rg-externaldns"
-location="uksouth"
-zone="thehypepipe.co.uk"
-dns_service_principle="external_dns"
+RESOURCE_GROUP="rg-externaldns"
+LOCATION="uksouth"
+DNS_ZONE="thehypepipe.co.uk"
+DNS_SERVICE_PRINCIPLE="sp_external_dns"
+DNS_MANAGED_IDENTITY="mid_external_dns"
+AKS_RG_NAME="rush-rg-aks-dev-001"
+AKS_CLUSTER_NAME="rush-aks-001"
 
-# create resource group and zone
-az group create --name $resource_group --location $location
-az network dns zone create --resource-group $resource_group --name $zone
+# create resource group and dns zone
+az group create --name $RESOURCE_GROUP --location $LOCATION
+az network dns zone create --resource-group $RESOURCE_GROUP --name $DNS_ZONE
 
+
+
+#region [OPTION 1] Service Principle authentication
+# https://github.com/kubernetes-sigs/external-dns/blob/master/docs/tutorials/azure.md#creating-service-principal
 # create service principle for dns
-az ad sp create-for-rbac --name $dns_service_principle
+service_principle_json=$(az ad sp create-for-rbac --name $DNS_SERVICE_PRINCIPLE --skip-assignment)
+
+# prepare credential json
+assignee_id=$(echo "$service_principle_json" | jq -r .appId)
+cred_json=$(echo "$service_principle_json" | jq ". | {aadClientId: .appId, aadClientSecret: .password, tenantId: .tenant}")
+subscription_id=$(az account show --query "id" --output tsv)
+extra_json=$(cat <<-END
+{
+  "subscriptionId": "$subscription_id",
+  "resourceGroup": "$RESOURCE_GROUP"
+}
+END
+)
+
+# show separate json
+echo "$cred_json"
+echo "$extra_json"
+
+# combine into an array
+echo "$cred_json $extra_json" | jq -s
+
+# merge json
+echo "$cred_json $extra_json" | jq -s add
 
 # create config file in this format, then save as GitHub Secret (EXTERNAL_DNS_CREDENTIAL_JSON)
+# aadClientId = appId = $assignee_id
 # {
 #   "tenantId": "01234abc-de56-ff78-abc1-234567890def",
 #   "subscriptionId": "01234abc-de56-ff78-abc1-234567890def",
@@ -28,18 +58,58 @@ az ad sp create-for-rbac --name $dns_service_principle
 #   "aadClientId": "01234abc-de56-ff78-abc1-234567890def",
 #   "aadClientSecret": "01234abc-de56-ff78-abc1-234567890def"
 # }
+#endregion [OPTION 1] Service Principle authentication
 
-# find resource ids of the resource group where the dns zone is deployed, and the dns zone
-az group show --name $resource_group
-az network dns zone show --resource-group $resource_group --name $zone
+
+
+#region [OPTION 2] Managed Identity authentication
+# https://github.com/kubernetes-sigs/external-dns/blob/master/docs/tutorials/azure.md#azure-managed-service-identity-msi
+# create managed identity for dns
+assignee_id=$(az identity create --resource-group $RESOURCE_GROUP --name $DNS_MANAGED_IDENTITY --query "principalId" --output tsv)
+
+# get existing managed identity
+assignee_id=$(az ad sp list --display-name "$DNS_MANAGED_IDENTITY" --query [].objectId --output tsv)
+
+# create config file in this format, then save as GitHub Secret (EXTERNAL_DNS_CREDENTIAL_JSON)
+# {
+#   "tenantId": "01234abc-de56-ff78-abc1-234567890def",
+#   "subscriptionId": "01234abc-de56-ff78-abc1-234567890def",
+#   "resourceGroup": "MyDnsResourceGroup",
+#   "useManagedIdentityExtension": true
+# }
+
+# Get AKS node resource group name
+node_resource_group=$(az aks show --resource-group "$AKS_RG_NAME" --name "$AKS_CLUSTER_NAME" --query nodeResourceGroup -o tsv)
+
+# Get VMSS
+vmss_name=$(az vmss list --resource-group "$node_resource_group" --query "[].name" -o tsv)
+
+# Assign Managed Identity to VMSS
+managed_identity_id=$(az ad sp list --display-name "$DNS_MANAGED_IDENTITY" --query [].alternativeNames[1] --output tsv)
+az vmss identity assign --resource-group "$node_resource_group" --name "$vmss_name" --identities "$managed_identity_id"
+
+# Show VMSS identities
+az vmss identity show --resource-group "$node_resource_group" --name "$vmss_name"
+#endregion [OPTION 2] Managed Identity authentication
+
+
+
+#region Role Assignment
+# get resource ids of resource group and dns zone
+resource_group_id=$(az group show --name $RESOURCE_GROUP --query "id" --output tsv)
+dns_zone_id=$(az network dns zone show --resource-group $RESOURCE_GROUP --name $DNS_ZONE --query "id" --output tsv)
 
 # assign the rights to the created service principal, using the resource ids from previous step
 # 1. as a reader to the resource group
-az role assignment create --role "Reader" --assignee <appId GUID> --scope <resource group resource id>
+az role assignment create --role "Reader" --assignee "$assignee_id" --scope "$resource_group_id"
 
 # 2. as a contributor to DNS Zone itself
-az role assignment create --role "Contributor" --assignee <appId GUID> --scope <dns zone resource id>
+az role assignment create --role "Contributor" --assignee "$assignee_id" --scope "$dns_zone_id"
+#endregion Role Assignment
 
+
+
+#region Kubernetes
 # create kubernetes secret
 # first, write file from GitHub secret: echo $EXTERNAL_DNS_CREDENTIAL_JSON > ./creds/azure.json
 kubectl ns ingress
@@ -50,9 +120,12 @@ kubectl apply -f ./manifests/external-dns.yml
 
 # add arg to nginx-ingress deployment
 --publish-service=ingress/nginx-nginx-ingress-controller
+#endregion Kubernetes
+
+
 
 # testing
-az network dns record-set a list --resource-group $resource_group --zone $zone
+az network dns record-set a list --resource-group $RESOURCE_GROUP --zone $DNS_ZONE
 nslookup -type=SOA nexus.thehypepipe.co.uk
 nslookup nexus.thehypepipe.co.uk
 ping nexus.thehypepipe.co.uk
