4035f228bc490e0eb810fe39f141d550cdf8108c
Adam Rush <adamrushuk@gmail.com>
2020-05-24 09:09:02 +0100

    Removed array indices

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -22,7 +22,7 @@ resource "azuread_application" "aks_dns_sp" {
 }
 
 resource "azuread_service_principal" "aks_dns_sp" {
-  application_id = azuread_application.aks_dns_sp[0].application_id
+  application_id = azuread_application.aks_dns_sp.application_id
 }
 
 resource "random_string" "aks_dns_sp" {
@@ -34,8 +34,8 @@ resource "random_string" "aks_dns_sp" {
 }
 
 resource "azuread_service_principal_password" "aks_dns_sp" {
-  service_principal_id = azuread_service_principal.aks_dns_sp[0].id
-  value                = random_string.aks_dns_sp[0].result
+  service_principal_id = azuread_service_principal.aks_dns_sp.id
+  value                = random_string.aks_dns_sp.result
   end_date_relative    = "8760h" # 8760h = 1 year
 
   lifecycle {
@@ -72,8 +72,8 @@ resource "kubernetes_secret" "external_dns" {
   data = {
     cloud = <<EOT
     {
-      "aadClientId": "${azuread_service_principal.aks_dns_sp[0].application_id}",
-      "aadClientSecret": "${random_string.aks_dns_sp[0].result}",
+      "aadClientId": "${azuread_service_principal.aks_dns_sp.application_id}",
+      "aadClientSecret": "${random_string.aks_dns_sp.result}",
       "tenantId": "${data.azurerm_subscription.current.tenant_id}",
       "subscriptionId": "${data.azurerm_subscription.current.subscription_id}",
       "resourceGroup": "${azurerm_resource_group.dns.name}"
