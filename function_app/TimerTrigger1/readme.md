04678d1754778deafb8263d95437f1d9b2a4e981
Adam Rush <adamrushuk@gmail.com>
2020-07-15 07:58:36 +0100

    Added check for running VMs

--- function_app/TimerTrigger1/readme.md
+++ function_app/TimerTrigger1/readme.md
@@ -6,6 +6,31 @@ The `TimerTrigger` makes it incredibly easy to have your functions executed on a
 
 For a `TimerTrigger` to work, you provide a schedule in the form of a [cron expression](https://en.wikipedia.org/wiki/Cron#CRON_expression)(See the link for full details). A cron expression is a string with 6 separate expressions which represent a given schedule via patterns. The pattern we use to represent every 5 minutes is `0 */5 * * * *`. This, in plain text, means: "When seconds is equal to 0, minutes is divisible by 5, for any hour, day of the month, month, day of the week, or year".
 
-## Learn more
+## Local Testing
 
-<TODO> Documentation
+Full instructions here: https://docs.microsoft.com/en-us/azure/azure-functions/functions-develop-vs-code?tabs=csharp#run-functions-locally
+
+Ensure the [Azure Functions Core Tools are installed](https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=windows%2Ccsharp%2Cbash#install-the-azure-functions-core-tools), 
+
+```powershell
+# install prereqs
+choco install dotnetcore-sdk
+choco install azure-functions-core-tools-3 --params "'/x64'"
+```
+
+then follow the steps below to run and test
+the code locally:
+
+```powershell
+<# set to use PowerShell 7
+$env:FUNCTIONS_WORKER_RUNTIME_VERSION = '~7'
+#>
+# enter function directory
+cd function_app
+
+# install extensions
+func extensions install
+
+# start function
+func host start
+```
--- function_app/TimerTrigger1/run.ps1
+++ function_app/TimerTrigger1/run.ps1
@@ -12,6 +12,56 @@ if ($Timer.IsPastDue) {
 # Write an information log with the current time.
 Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
 
+<#
+# Local Debugging, as proper container is currently broken
+# Load env vars from local.settings.json
+$jsonSettings = Get-Content "function_app\local.settings.json" -Raw | ConvertFrom-Json
+$envVars = $jsonSettings.Values
+foreach ($envVar in $envVars.PSObject.Properties) {
+    New-Item -Path "Env:$($envVar.Name)" -Value $envVar.Value -Force
+}
+
+gci env:
+
+$PSVersionTable | Out-String
+
+# Needed for VSCode debugger
+# Wait-Debugger
+#>
+
 
 # List VMSS
-Get-AzVmss
+Write-Host "Getting VMSS VMs..."
+$vmssVms = Get-AzVmss | Get-AzVmssVM -InstanceView | Select-Object Name, ResourceGroupName, ProvisioningState, @{ N = "PowerState"; E = { $_.InstanceView.Statuses[1].Code } }
+$vmssVms
+
+# Check for running VMs
+$runningVmssVms = $vmssVms | Where-Object { $_.PowerState -eq "PowerState/running" }
+
+Write-Host "Running VM count: [$($runningVmssVms.Count)]"
+
+# Sent notification if VMs still running
+if ($runningVmssVms.Count -gt 0) {
+
+    $vmsRunningwithinAllowedTimeRange = Test-WithinAllowedTimeRange -TimeRange $env:WEEKDAY_ALLOWED_TIME_RANGE
+    Write-Host "Are VMs running within allowed time range of [$env:WEEKDAY_ALLOWED_TIME_RANGE]?...[$vmsRunningwithinAllowedTimeRange]"
+
+    if (-not $vmsRunningwithinAllowedTimeRange) {
+        Write-Host "Sending IFTTT notification as [$($runningVmssVms.Count)] VMs still running..."
+
+        $params = @{
+            EventName = "azure_vm_check"
+            Key       = $env:IFTTT_WEBHOOK_KEY
+            Value1    = "Allowed time range: [$env:WEEKDAY_ALLOWED_TIME_RANGE]"
+            Value2    = $runningVmssVms.Name -join ", "
+            Value3    = ($runningVmssVms.ResourceGroupName | Select-Object -Unique) -join ", "
+        }
+        Send-IftttAppNotification @params
+
+        Start-Sleep -Seconds 5
+    } else {
+        Write-Host "Skipping sending notification as: [VMs running within allowed time range]"
+    }
+} else {
+    Write-Host "Skipping sending notification as: [0 VMs running]"
+}
