67eac260c857bca28dd4d492cac201b144300510
Adam Rush <adamrushuk@gmail.com>
2020-07-23 08:12:53 +0100

    Added Function App zip and zip creation script

---
+++ function_app/CreateFunctionAppZip.ps1
@@ -0,0 +1,16 @@
+# Creates a zip file for Function App files
+
+# Vars
+$functionZipPath = Join-Path -Path $PSScriptRoot -ChildPath "..\terraform\files\function_app.zip"
+$excludeList = Get-Content -Path "function_app\.funcignore"
+$filesToZip = @()
+
+# Prepare files to zip
+foreach ($file in (Get-ChildItem -Path $PSScriptRoot)) {
+    if ($file.name -notin $excludeList) {
+        $filesToZip += $file.fullname
+    }
+}
+
+# Create zip file of Function App
+Compress-Archive -Path $filesToZip -DestinationPath $functionZipPath -Force
