986c18e8774a1275dce4674655262307e2bfaaaf
Adam Rush <adamrushuk@gmail.com>
2020-08-09 12:47:21 +0100

    Testing AKS upgrade to v1.16.13

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,8 +6,8 @@
 variable "kubernetes_version" {
   # lowest v1.15: 1.15.11
   # current default: 1.16.13
-  default = "1.15.11"
-  # default = "1.16.13"
+  # default = "1.15.11"
+  default = "1.16.13"
 }
 
 # Helm charts
