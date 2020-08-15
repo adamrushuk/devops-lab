f33b4e96dbc4a648d169828e99b6f04e3913ef1c
Adam Rush <adamrushuk@gmail.com>
2020-08-15 07:45:21 +0100

    Disabled velero

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -149,7 +149,7 @@ variable "agent_pool_profile_disk_size_gb" {
 # Velero
 variable "velero_enabled" {
   description = "Should Velero be enabled"
-  default     = true
+  default     = false
 }
 
 variable "velero_resource_group_name" {
