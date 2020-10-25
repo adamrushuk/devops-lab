0c348dae6b788156194c4a10cf3e6ddc3cbb8b07
Adam Rush <adamrushuk@gmail.com>
2020-10-25 08:40:01 +0000

    Added velero_mi_aks_node_rg_vm_contributor

--- terraform/velero_mi_auth.tf
+++ terraform/velero_mi_auth.tf
@@ -14,3 +14,13 @@ resource "azurerm_role_assignment" "velero_mi_velero_storage_rg" {
   role_definition_name             = "Contributor"
   scope                            = azurerm_resource_group.velero[0].id
 }
+
+# https://docs.microsoft.com/en-us/azure/role-based-access-control/built-in-roles?source=docs#virtual-machine-contributor
+# vm disk read and write action / perms
+resource "azurerm_role_assignment" "velero_mi_aks_node_rg_vm_contributor" {
+  count                            = var.velero_enabled ? 1 : 0
+  principal_id                     = azurerm_user_assigned_identity.velero[0].principal_id
+  role_definition_name             = "Virtual Machine Contributor"
+  scope                            = data.azurerm_resource_group.aks.id
+  skip_service_principal_aad_check = true
+}
