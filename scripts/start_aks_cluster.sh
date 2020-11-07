eddf44cb85c2eee44b4da27e15376e1f64e1b423
Adam Rush <adamrushuk@gmail.com>
2020-11-07 09:57:43 +0000

    Added log output

--- scripts/start_aks_cluster.sh
+++ scripts/start_aks_cluster.sh
@@ -15,8 +15,10 @@ az extension update --name aks-preview
 
 # Check AKS power state
 aks_power_state=$(az aks show --name "$AKS_CLUSTER_NAME" --resource-group "$AKS_RG_NAME" --query 'powerState.code')
+echo -e "\n[$AKS_CLUSTER_NAME] AKS Cluster power state is [$aks_power_state]."
+
 if [ "$aks_power_state" == "Running" ]; then
-    echo -e "\nSKIPPING: $AKS_CLUSTER_NAME AKS Cluster state is [$aks_power_state]."
+    echo -e "\nSKIPPING: $AKS_CLUSTER_NAME AKS Cluster is already running."
 else
     # Start AKS Cluster
     message="Starting AKS Cluster: [$AKS_CLUSTER_NAME]"
