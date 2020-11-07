93c126af7d57452ef2c0e9fbc5781fc7137f32a7
Adam Rush <adamrushuk@gmail.com>
2020-11-07 10:16:37 +0000

    Added power state check to aks stop script

--- scripts/stop_aks_cluster.sh
+++ scripts/stop_aks_cluster.sh
@@ -13,8 +13,16 @@ az extension add --name aks-preview
 # Update the extension to make sure you have the latest version installed
 az extension update --name aks-preview
 
-# Stop AKS Cluster
-message="Stopping AKS Cluster: [$AKS_CLUSTER_NAME]"
-echo -e "\nSTARTED: $message..."
-az aks stop --name "$AKS_CLUSTER_NAME" --resource-group "$AKS_RG_NAME"
-echo -e "FINISHED: $message."
+# Check AKS power state
+aks_power_state=$(az aks show --name "$AKS_CLUSTER_NAME" --resource-group "$AKS_RG_NAME" --output tsv --query 'powerState.code')
+echo -e "\n[$AKS_CLUSTER_NAME] AKS Cluster power state is [$aks_power_state]."
+
+if [ "$aks_power_state" == "Stopped" ]; then
+    echo -e "\nSKIPPING: $AKS_CLUSTER_NAME AKS Cluster is already stopped."
+else
+    # Stop AKS Cluster
+    message="Stopping AKS Cluster: [$AKS_CLUSTER_NAME]"
+    echo -e "\nSTARTED: $message..."
+    az aks stop --name "$AKS_CLUSTER_NAME" --resource-group "$AKS_RG_NAME"
+    echo -e "FINISHED: $message."
+fi
