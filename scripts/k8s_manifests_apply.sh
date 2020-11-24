5c307b8543059441a90b45380ccf6877beebaab5
Adam Rush <adamrushuk@gmail.com>
2020-11-24 08:12:28 +0000

    Added Weave Scope

--- scripts/k8s_manifests_apply.sh
+++ scripts/k8s_manifests_apply.sh
@@ -26,6 +26,10 @@ kubectl version --short
 # message="Applying Kubernetes manifests"
 # echo "STARTED: $message..."
 
+# Install Weave Scope
+# https://www.weave.works/docs/scope/latest/installing/#k8s
+kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
 # # external-dns
 # kubectl apply -n ingress -f ./manifests/external-dns.yml
 # echo "FINISHED: $message."
