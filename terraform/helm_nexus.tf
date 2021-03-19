4d0dd3bc75e3a74619da9c6c667f01f9f6e5f0c5
Adam Rush <adamrushuk@gmail.com>
2021-03-19 09:18:22 +0000

    Changed nexus_cert_sync resource location

--- terraform/helm_nexus.tf
+++ terraform/helm_nexus.tf
@@ -12,6 +12,28 @@ resource "kubernetes_namespace" "nexus" {
   depends_on = [module.aks]
 }
 
+# https://www.terraform.io/docs/provisioners/local-exec.html
+resource "null_resource" "nexus_cert_sync" {
+  triggers = {
+    # always_run = "${timestamp()}"
+    cert_sync_yaml_contents = filemd5(var.nexus_cert_sync_yaml_path)
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOT
+      export KUBECONFIG=${var.aks_config_path}
+      kubectl apply -f ${var.nexus_cert_sync_yaml_path}
+    EOT
+  }
+
+  depends_on = [
+    local_file.kubeconfig,
+    helm_release.akv2k8s,
+    kubernetes_namespace.nexus
+  ]
+}
+
 # https://www.terraform.io/docs/providers/helm/r/release.html
 resource "helm_release" "nexus" {
   chart      = "sonatype-nexus"
