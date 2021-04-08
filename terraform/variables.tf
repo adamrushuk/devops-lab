b685b6a2d28efced4ea41fe6560d8caf2691b6ce
Adam Rush <adamrushuk@gmail.com>
2021-04-08 07:45:13 +0100

    Bumped argocd to v2.0.0

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -91,12 +91,12 @@ variable "kured_image_tag" {
 # https://github.com/argoproj/argo-helm/blob/master/charts/argo-cd/Chart.yaml#L5
 # helm search repo argo/argo-cd
 variable "argocd_chart_version" {
-  default = "2.17.5"
+  default = "3.0.0"
 }
 
 # https://hub.docker.com/r/argoproj/argocd/tags
 variable "argocd_image_tag" {
-  default = "v1.8.7"
+  default = "v2.0.0"
 }
 #endregion Versions
 
