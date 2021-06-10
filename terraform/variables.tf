a1ebe88e23f69bc2680416609369a32d41cf460c
Adam Rush <adamrushuk@gmail.com>
2021-06-10 07:42:51 +0100

    Changed chart / binary versions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,7 +20,7 @@ variable "kubernetes_version" {
 # helm repo update
 # helm search repo ingress-nginx/ingress-nginx
 variable "nginx_chart_version" {
-  default = "3.32.0"
+  default = "3.33.0"
 }
 
 # https://hub.helm.sh/charts/jetstack/cert-manager
@@ -33,7 +33,7 @@ variable "cert_manager_chart_version" {
 # helm search repo vmware-tanzu/velero
 # * also update terraform/helm/velero_values.yaml
 variable "velero_chart_version" {
-  default = "2.21.1"
+  default = "2.22.0"
 }
 
 # https://hub.docker.com/r/velero/velero/tags
@@ -71,7 +71,7 @@ variable "aad_pod_identity_chart_version" {
 # https://github.com/bitnami/charts/blob/master/bitnami/external-dns/Chart.yaml#L21
 # helm search repo bitnami/external-dns
 variable "external_dns_chart_version" {
-  default = "5.0.2"
+  default = "5.0.3"
 }
 
 # https://github.com/weaveworks/kured/tree/master/charts/kured
@@ -90,7 +90,7 @@ variable "kured_image_tag" {
 # https://github.com/argoproj/argo-helm/blob/master/charts/argo-cd/Chart.yaml#L5
 # helm search repo argo/argo-cd
 variable "argocd_chart_version" {
-  default = "3.6.6"
+  default = "3.6.8"
 }
 
 # https://hub.docker.com/r/argoproj/argocd/tags
