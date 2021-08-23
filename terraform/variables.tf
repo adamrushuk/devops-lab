38031eae215acea4f949778e21b1426ca8b360b7
Adam Rush <adamrushuk@gmail.com>
2021-08-23 08:11:03 +0100

    Bumped chart / binary versions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,30 +20,30 @@ variable "kubernetes_version" {
 # helm repo update
 # helm search repo ingress-nginx/ingress-nginx
 variable "nginx_chart_version" {
-  default = "3.34.0"
+  default = "3.36.0"
 }
 
 # https://hub.helm.sh/charts/jetstack/cert-manager
 # helm search repo jetstack/cert-manager
 variable "cert_manager_chart_version" {
-  default = "v1.4.1"
+  default = "v1.5.2"
 }
 
 # https://github.com/vmware-tanzu/helm-charts/releases
 # helm search repo vmware-tanzu/velero
 # * also update terraform/helm/velero_values.yaml
 variable "velero_chart_version" {
-  default = "2.23.5"
+  default = "2.23.6"
 }
 
 # https://hub.docker.com/r/velero/velero/tags
 variable "velero_image_tag" {
-  default = "v1.6.2"
+  default = "v1.6.3"
 }
 
 # https://hub.docker.com/r/sonatype/nexus3/tags
 variable "nexus_image_tag" {
-  default = "3.32.0"
+  default = "3.33.1"
 }
 
 # https://github.com/adamrushuk/charts/releases
@@ -58,7 +58,7 @@ variable "nexus_chart_version" {
 # https://github.com/SparebankenVest/public-helm-charts/blob/master/stable/akv2k8s/Chart.yaml#L5
 # helm search repo spv-charts/akv2k8s
 variable "akv2k8s_chart_version" {
-  default = "2.0.11"
+  default = "2.1.0"
 }
 
 # https://github.com/Azure/aad-pod-identity/blob/master/charts/aad-pod-identity/Chart.yaml#L4
@@ -71,13 +71,13 @@ variable "aad_pod_identity_chart_version" {
 # https://github.com/bitnami/charts/blob/master/bitnami/external-dns/Chart.yaml#L21
 # helm search repo bitnami/external-dns
 variable "external_dns_chart_version" {
-  default = "5.2.2"
+  default = "5.4.1"
 }
 
 # https://github.com/weaveworks/kured/tree/master/charts/kured
 # helm search repo kured/kured
 variable "kured_chart_version" {
-  default = "2.8.0"
+  default = "2.9.0"
 }
 
 # https://github.com/weaveworks/kured#kubernetes--os-compatibility
@@ -90,7 +90,7 @@ variable "kured_image_tag" {
 # https://github.com/argoproj/argo-helm/blob/master/charts/argo-cd/Chart.yaml#L5
 # helm search repo argo/argo-cd
 variable "argocd_chart_version" {
-  default = "3.11.1"
+  default = "3.12.1"
 }
 
 # https://hub.docker.com/r/argoproj/argocd/tags
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -23,7 +23,7 @@ terraform {
     # https://github.com/terraform-providers/terraform-provider-azurerm/releases
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.70.0"
+      version = "~> 2.73.0"
     }
 
     # https://github.com/terraform-providers/terraform-provider-azuread/releases
@@ -35,7 +35,7 @@ terraform {
     # https://github.com/hashicorp/terraform-provider-kubernetes/releases
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.3.2"
+      version = "~> 2.4.1"
     }
 
     # https://github.com/hashicorp/terraform-provider-helm/releases
