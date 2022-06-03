d91845d3e8bbe98f1ca64265d025e11bae5e6e4d
Adam Rush <adamrushuk@gmail.com>
2022-06-03 09:04:54 +0100

    Fixed LAW SKU

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -24,7 +24,7 @@ resource "azurerm_log_analytics_workspace" "aks" {
   name                = var.log_analytics_workspace_name
   location            = azurerm_resource_group.aks.location
   resource_group_name = azurerm_resource_group.aks.name
-  sku                 = "Free"
+  sku                 = "PerGB2018"
   retention_in_days   = 7
   tags                = var.tags
 
