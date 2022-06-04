3d0d2cfee79aa5febc69995b113592da8c82a6e1
Adam Rush <adamrushuk@gmail.com>
2022-06-04 10:45:21 +0100

    Disabled func app PS7 update - only restarts

--- scripts/Fix-FunctionApp.ps1
+++ scripts/Fix-FunctionApp.ps1
@@ -1,19 +1,17 @@
 # source: https://github.com/terraform-providers/terraform-provider-azurerm/issues/8867#issuecomment-849842849
 
-# Fixes PowerShell function app stack version to 7
+# Fixes PowerShell function app stack version to 7, and restarts
 
-# Change function app to PS 7
-Write-Host 'Change function app to PS 7'
 $function = az functionapp show --name $env:FUNCTION_APP_NAME --resource-group $env:FUNCTION_APP_RG | ConvertFrom-Json
 
 if ($function.siteConfig.powerShellVersion -ne "~7") {
-    Write-Host "Updating powershell version to ~7..."
-    az functionapp update --name $env:FUNCTION_APP_NAME --resource-group $env:FUNCTION_APP_RG --set "siteConfig.powerShellVersion=~7"
-
-    # Restart Function App
-    # az functionapp restart --name $env:FUNCTION_APP_NAME --resource-group $env:FUNCTION_APP_RG
+    Write-Host "[NoOp] Updating powershell version to ~7..."
+    # az functionapp update --name $env:FUNCTION_APP_NAME --resource-group $env:FUNCTION_APP_RG --set "siteConfig.powerShellVersion=~7"
 } else {
     Write-Host "Powershell version already set to to ~7"
 }
+
+# Restart Function App
+az functionapp restart --name $env:FUNCTION_APP_NAME --resource-group $env:FUNCTION_APP_RG
 
 Write-Host ''
