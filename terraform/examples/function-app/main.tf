2aba2bf577fc88a4ae209b5144ccb3e71bd0694a
Adam Rush <adamrushuk@gmail.com>
2022-06-04 09:39:46 +0100

    Fixed checkov checks - added some skip codes

--- terraform/examples/function-app/main.tf
+++ terraform/examples/function-app/main.tf
@@ -36,17 +36,19 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = local.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2"
+  name                      = local.storage_account_name
+  resource_group_name       = azurerm_resource_group.example.name
+  location                  = azurerm_resource_group.example.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
+  min_tls_version           = "TLS1_2"
 }
 
 resource "azurerm_storage_container" "example" {
-  name                 = local.storage_container_name
-  storage_account_name = azurerm_storage_account.example.name
+  name                  = local.storage_container_name
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
 }
 
 data "archive_file" "example" {
