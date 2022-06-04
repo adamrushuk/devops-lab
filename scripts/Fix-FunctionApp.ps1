f01667a732b7d986ae5940abd97e41ceb48b2be6
Adam Rush <adamrushuk@gmail.com>
2022-06-04 11:15:32 +0100

    Added comments

--- scripts/Fix-FunctionApp.ps1
+++ scripts/Fix-FunctionApp.ps1
@@ -4,6 +4,7 @@
 
 $function = az functionapp show --name $env:FUNCTION_APP_NAME --resource-group $env:FUNCTION_APP_RG | ConvertFrom-Json
 
+# TODO: I dont think this is required anymore as I use application_stack > powershell_core_version
 if ($function.siteConfig.powerShellVersion -ne "~7") {
     Write-Host "[NoOp] Updating powershell version to ~7..."
     # az functionapp update --name $env:FUNCTION_APP_NAME --resource-group $env:FUNCTION_APP_RG --set "siteConfig.powerShellVersion=~7"
@@ -12,6 +13,7 @@ if ($function.siteConfig.powerShellVersion -ne "~7") {
 }
 
 # Restart Function App
+Write-Host "Restarting function app [$($env:FUNCTION_APP_NAME)]..."
 az functionapp restart --name $env:FUNCTION_APP_NAME --resource-group $env:FUNCTION_APP_RG
 
-Write-Host ''
+Write-Host 'FINISHED.'
