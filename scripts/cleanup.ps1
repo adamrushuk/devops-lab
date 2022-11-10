070ef20c00e4e738846d579ba4aa03c9e4b30efe
Adam Rush <adamrushuk@gmail.com>
2022-11-10 08:38:23 +0000

    Fixed cleanup

--- scripts/cleanup.ps1
+++ scripts/cleanup.ps1
@@ -1,4 +1,3 @@
-# TODO: delete this file if not required
 # cleanup all resource groups
 # useful after failed build/destroy workflows
 
@@ -9,10 +8,6 @@ param(
     $ResourceGroupPrefix
 )
 
-# Write-Output "Authenticating PowerShell sessions using env vars..."
-# $servicePrincipleCredential = [pscredential]::new($env:ARM_CLIENT_ID, (ConvertTo-SecureString $env:ARM_CLIENT_SECRET -AsPlainText -Force))
-# Connect-AzAccount -ServicePrincipal -Tenant $env:ARM_TENANT_ID -Credential $servicePrincipleCredential -Subscription $env:ARM_SUBSCRIPTION_ID -Verbose
-
 $taskMessage = "Deleting all devops lab resource groups"
 Write-Output "STARTED: $taskMessage..."
 
@@ -21,7 +16,7 @@ $resourceGroupsToDelete = Get-AzResourceGroup -Name "$ResourceGroupPrefix*"
 $resourceGroupsToDelete.ResourceGroupName
 
 Write-Output "Deleting 'AsJob' for async removal..."
-# $jobs = $resourceGroupsToDelete | Remove-AzResourceGroup -Force -AsJob
+$jobs = $resourceGroupsToDelete | Remove-AzResourceGroup -Force -AsJob
 
 Write-Output "Waiting for [$($jobs.Count)] jobs to finish..."
 $jobs | Wait-Job
